ae277b0a404f6c3f3c4a0eeaf89bdbde2e16f019
Vi Ramen <vi.is.chapmann@gmail.com>
2026-06-29 06:04:48 +0300

    Initial commit

--- src/lib.rs
+++ src/lib.rs
@@ -118,3 +118,19 @@ pub use arc::*;
 
 mod rc;
 pub use rc::*;
+
+pub macro hash($s:expr) {{
+    const fn fnv1a64(data: &[u8]) -> u64 {
+        const FNV_OFFSET_BASIS: u64 = 0xcbf29ce484222325;
+        const FNV_PRIME: u64 = 0x100000001b3;
+        let mut hash = FNV_OFFSET_BASIS;
+        let mut i = 0;
+        while i < data.len() {
+            hash ^= data[i] as u64;
+            hash = hash.wrapping_mul(FNV_PRIME);
+            i += 1;
+        }
+        hash
+    }
+    fnv1a64($s)
+}}
